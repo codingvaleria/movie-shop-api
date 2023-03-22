82990384dafc1cf7399dd5fca9b69995634a2b22
codingvaleria <codingvaleria@gmail.com>
2023-03-22 09:04:18 +0300

    Add CRUD actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,60 @@
 class MoviesController < ApplicationController
+    wrap_parameters format:[]
+    #GET/movies
+    def index
+        movies = Movie.all
+        render json: movies
+    end
+
+    #GET /movies/:id
+    def show
+        movie = Movie.find_by(id: params[:id])
+        if movie
+            render json: movie
+        else
+            render json: { error: "Movie not found" }, status: :not_found
+        end
+    end
+
+    #POST/movies
+    def create
+        movie = Movie.create(movie_params)
+        render json: movie, status: :created
+    end
+
+    def update  
+        #find
+        movie = Movie.find_by(id: params[:id])
+        if movie
+         #update
+            movie.update(movie_params)
+            render json: movie, status: :accepted
+        else
+            render json: {error: "movie not found"}, status: :not_found
+        end
+
+    end
+
+    def destroy
+        #find
+        movie = Movie.find_by(id: params[:id])
+        if movie
+            #delete
+            movie.destroy
+            head :no_content
+        else
+            render json: {error: "movie not found"}, status: :not_found
+        end
+
+    end
+
+    private
+    def movie_params
+        params.permit(:title, :release_year, :price, :image, :description, :category)
+    end
+
+
+  
+
+
 end
